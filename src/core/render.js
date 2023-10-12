25638d5b5bd2ad5d05260d317ed3c6dca6b888ee
AbianS <abiansuarezbrito@gmail.com>
2023-10-12 14:35:00 +0100

    style: add prettier

--- src/core/render.js
+++ src/core/render.js
@@ -4,11 +4,11 @@
  * @returns {string} The DML diagram as a string.
  */
 function renderDml(dmmf) {
-	const diagram = "erDiagram";
-	const dml = dmmf.datamodel;
-	const classes = generateClasses(dml.models);
-	const relationships = generateRelationships(dml.models);
-	return diagram + "\n" + classes + "\n" + relationships;
+  const diagram = "erDiagram"
+  const dml = dmmf.datamodel
+  const classes = generateClasses(dml.models)
+  const relationships = generateRelationships(dml.models)
+  return diagram + "\n" + classes + "\n" + relationships
 }
 
 /**
@@ -18,23 +18,22 @@ function renderDml(dmmf) {
  * @returns {string} - A string of classes.
  */
 function generateClasses(models) {
-	return models
-		.map((model) => {
-			const fields = model.fields
-				.filter(
-					(field) =>
-						field.kind !== "object" &&
-						!model.fields.find(
-							({ relationFromFields }) =>
-								relationFromFields &&
-								relationFromFields.includes(field.name)
-						)
-				)
-				.map((field) => `    ${field.type} ${field.name}`)
-				.join("\n");
-			return `  ${model.name} {\n${fields}\n  }`;
-		})
-		.join("\n\n");
+  return models
+    .map((model) => {
+      const fields = model.fields
+        .filter(
+          (field) =>
+            field.kind !== "object" &&
+            !model.fields.find(
+              ({ relationFromFields }) =>
+                relationFromFields && relationFromFields.includes(field.name),
+            ),
+        )
+        .map((field) => `    ${field.type} ${field.name}`)
+        .join("\n")
+      return `  ${model.name} {\n${fields}\n  }`
+    })
+    .join("\n\n")
 }
 
 /**
@@ -43,50 +42,47 @@ function generateClasses(models) {
  * @returns {string} - A string representation of relationships between models.
  */
 function generateRelationships(models) {
-	// Initialize an array to store the relationships
-	const relationships = models.reduce((acc, model) => {
-		// Iterate through the models
-		const modelRelationships = model.fields.reduce((modelAcc, field) => {
-			if (
-				field.relationFromFields &&
-				field.relationFromFields.length > 0
-			) {
-				// If a field is a relationship, generate its string representation
-				const relationshipName = field.name;
-				const thisSide = model.name;
-				const otherSide = field.type;
+  // Initialize an array to store the relationships
+  const relationships = models.reduce((acc, model) => {
+    // Iterate through the models
+    const modelRelationships = model.fields.reduce((modelAcc, field) => {
+      if (field.relationFromFields && field.relationFromFields.length > 0) {
+        // If a field is a relationship, generate its string representation
+        const relationshipName = field.name
+        const thisSide = model.name
+        const otherSide = field.type
 
-				// Determine the multiplicity on both sides of the relationship
-				let otherSideMultiplicity = field.isList
-					? "}o"
-					: !field.isRequired
-					? "|o"
-					: "||";
-				const otherModel = models.find((m) => m.name === otherSide);
-				const otherField = otherModel.fields.find(
-					({ relationName }) => relationName === field.relationName
-				);
+        // Determine the multiplicity on both sides of the relationship
+        let otherSideMultiplicity = field.isList
+          ? "}o"
+          : !field.isRequired
+          ? "|o"
+          : "||"
+        const otherModel = models.find((m) => m.name === otherSide)
+        const otherField = otherModel.fields.find(
+          ({ relationName }) => relationName === field.relationName,
+        )
 
-				let thisSideMultiplicity = otherField.isList
-					? "o{"
-					: !otherField.isRequired
-					? "o|"
-					: "||";
+        let thisSideMultiplicity = otherField.isList
+          ? "o{"
+          : !otherField.isRequired
+          ? "o|"
+          : "||"
 
-				// Build the representation of the relationship and add it to the array
-				modelAcc.push(
-					`    ${thisSide} ${thisSideMultiplicity}--${otherSideMultiplicity} ${otherSide} : "${relationshipName}"`
-				);
-			}
-			return modelAcc;
-		}, []);
+        // Build the representation of the relationship and add it to the array
+        modelAcc.push(
+          `    ${thisSide} ${thisSideMultiplicity}--${otherSideMultiplicity} ${otherSide} : "${relationshipName}"`,
+        )
+      }
+      return modelAcc
+    }, [])
 
-		// Combine the relationships from the current model with the accumulated relationships
-		return acc.concat(modelRelationships);
-	}, []);
+    // Combine the relationships from the current model with the accumulated relationships
+    return acc.concat(modelRelationships)
+  }, [])
 
-	// Return all relationships as a string, joining each relationship with line breaks
-	return relationships.join("\n");
+  // Return all relationships as a string, joining each relationship with line breaks
+  return relationships.join("\n")
 }
 
 /**
@@ -96,7 +92,7 @@ function generateRelationships(models) {
  * @returns {string} The generated HTML diagram.
  */
 function generateDiagram(dml, scriptUri) {
-	return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 	<html lang="en">
 		<head>
 			<meta charset="UTF-8" />
@@ -152,10 +148,10 @@ function generateDiagram(dml, scriptUri) {
 			</script>
 		</body>
 	</html>
-	`;
+	`
 }
 
 module.exports = {
-	renderDml,
-	generateDiagram,
-};
+  renderDml,
+  generateDiagram,
+}
--- src/extension.js
+++ src/extension.js
@@ -1,60 +1,58 @@
-const { getDMMF } = require("@prisma/internals");
-const vscode = require("vscode");
-const { renderDml, generateDiagram } = require("./core/render.js");
-const path = require("path");
+const { getDMMF } = require("@prisma/internals")
+const vscode = require("vscode")
+const { renderDml, generateDiagram } = require("./core/render.js")
+const path = require("path")
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const disposable = vscode.commands.registerCommand(
-		"prisma-generate-uml.generateUML",
-		async () => {
-			const editor = vscode.window.activeTextEditor;
+  const disposable = vscode.commands.registerCommand(
+    "prisma-generate-uml.generateUML",
+    async () => {
+      const editor = vscode.window.activeTextEditor
 
-			if (editor && editor.document.languageId === "prisma") {
-				const content = editor.document.getText();
+      if (editor && editor.document.languageId === "prisma") {
+        const content = editor.document.getText()
 
-				const response = await getDMMF({ datamodel: content });
-				const dml = renderDml(response);
+        const response = await getDMMF({ datamodel: content })
+        const dml = renderDml(response)
 
-				const panel = vscode.window.createWebviewPanel(
-					"prismaEr",
-					"Prisma Diagram",
-					vscode.ViewColumn.Two,
-					{
-						enableScripts: true,
-						localResourceRoots: [
-							vscode.Uri.file(
-								path.join(context.extensionPath, "src/core")
-							),
-						],
-					}
-				);
+        const panel = vscode.window.createWebviewPanel(
+          "prismaEr",
+          "Prisma Diagram",
+          vscode.ViewColumn.Two,
+          {
+            enableScripts: true,
+            localResourceRoots: [
+              vscode.Uri.file(path.join(context.extensionPath, "src/core")),
+            ],
+          },
+        )
 
-				const scriptUri = panel.webview.asWebviewUri(
-					vscode.Uri.file(
-						path.join(context.extensionPath, "src/core/mermaid.js")
-					)
-				);
+        const scriptUri = panel.webview.asWebviewUri(
+          vscode.Uri.file(
+            path.join(context.extensionPath, "src/core/mermaid.js"),
+          ),
+        )
 
-				const svgContent = generateDiagram(dml, scriptUri);
+        const svgContent = generateDiagram(dml, scriptUri)
 
-				panel.webview.html = svgContent;
-			} else {
-				vscode.window.showInformationMessage(
-					"Abre un archivo .prisma para usar este comando."
-				);
-			}
-		}
-	);
+        panel.webview.html = svgContent
+      } else {
+        vscode.window.showInformationMessage(
+          "Abre un archivo .prisma para usar este comando.",
+        )
+      }
+    },
+  )
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate,
-};
+  activate,
+  deactivate,
+}
