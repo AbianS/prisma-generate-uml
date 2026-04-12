02372af5d1a598d0e8f9227f78af3e3769737f5c
Abian Suarez <abiansuarezbrito@gmail.com>
2026-04-12 20:56:04 +0100

    feat(02-03): narrow postMessage to WebviewMessage in vscode-api.ts
    - Add import for WebviewMessage from '../types/messages'
    - Replace postMessage(message: any) with postMessage(message: WebviewMessage)
    - setState and getState retain any as they are out of scope

--- packages/webview-ui/src/lib/utils/vscode-api.ts
+++ packages/webview-ui/src/lib/utils/vscode-api.ts
@@ -1,5 +1,7 @@
+import type { WebviewMessage } from '../types/messages';
+
 interface VsCodeApi {
-  postMessage(message: any): void;
+  postMessage(message: WebviewMessage): void;
   setState(state: any): void;
   getState(): any;
 }
